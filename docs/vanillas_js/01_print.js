20a45b3de2310558ef07dee82114339925172aa8
sky <adsky0309@gmail.com>
2024-01-23 12:05:13 +0900

    first commit

---
+++ docs/vanillas_js/01_print.js
@@ -0,0 +1,24 @@
+// 변수 선언 let
+let name = "sky"
+// undefined
+console.log(name)
+
+
+let welcome = 'Hello World'
+// undefined
+welcome
+// 'Hello World'
+let numbers = 55
+// undefined
+numbers
+// 55
+
+let concat = 'String : ' + welcome + ', Number : ' +numbers
+// undefined
+concat
+// 'String : Hello World, Number : 55'
+
+let concats_tilt = `string : ${welcome}, Numbers : ${concat}`
+// undefined
+concats_tilt
+// 'string : Hello World, Numbers : String : Hello World, Number : 55'
