fb67bb5d63f747efc6883d75ebc2a45ce3167096
sky <adsky0309@gmail.com>
2024-01-24 09:17:23 +0900

    event quest

--- docs/vanillas_js/01_print.js
+++ docs/vanillas_js/01_print.js
@@ -1,24 +1,24 @@
 // 변수 선언 let
-let name = "sky"
+let name = "sky" ;
 // undefined
-console.log(name)
+console.log(name) ;
 
 
-let welcome = 'Hello World'
+let welcome = 'Hello World' ;
 // undefined
 welcome
 // 'Hello World'
-let numbers = 55
+let numbers = 55 ;
 // undefined
 numbers
 // 55
 
-let concat = 'String : ' + welcome + ', Number : ' +numbers
+let concat = 'String : ' + welcome + ', Number : ' +numbers ;
 // undefined
 concat
 // 'String : Hello World, Number : 55'
 
-let concats_tilt = `string : ${welcome}, Numbers : ${concat}`
+let concats_tilt = `string : ${welcome}, Numbers : ${concat}` ;
 // undefined
-concats_tilt
+concats_tilt ;
 // 'string : Hello World, Numbers : String : Hello World, Number : 55'
---
+++ docs/vanillas_js/02_conditions.js
@@ -0,0 +1,33 @@
+// condition 순서 : 변수
+5> 4 ;
+// True
+let first = 5;
+// undefined
+first > 4;
+// true
+first <4 ;
+// false
+
+
+// if
+if (true) { }
+    else if (true) {}
+    else {}
+
+if (numbers>0) {
+    console.log("숫자는 양수") ;}
+else if (numbers<0) {
+    console.log("숫자는 음수") ;}
+else {
+    console.log("숫자는 0");}
+
+// 삼항 연산자 (Ternary Operation)
+if (true) {
+} else {
+}
+
+result = (true) ? 'as true' : "as false" ;
+
+result = (numbers > 0) ? '양수' : "음수" ;
+// '양수'
+console.log(`숫자는 ${result}`);
